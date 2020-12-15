b2b7eb1410ef18d2be960d2d4894634b03792a40
Augusto Fabian Rodrigues <augustofabianrodrigues@gmail.com>
2020-12-15 18:50:33 -0300

    fix: Add a race with debouce for get forecast
    Without this race effect for debouncing, it would still cause the modal to open with stale data.

--- src/sagas/ui/forecast.js
+++ src/sagas/ui/forecast.js
@@ -1,22 +1,41 @@
-import { call, debounce, put, race, take } from 'redux-saga/effects';
+import { call, cancel, delay, fork, put, race, take } from 'redux-saga/effects';
 import * as forecastUIActions from '../../actions/ui/forecast';
 import * as forecastApi from '../../infra/api/forecast';
 
+export function* raceWithResetForecast(effect) {
+  return yield race({
+    result: effect,
+    reset: take(forecastUIActions.RESET_FORECAST),
+  });
+}
+
 export function* getForecast(action) {
-  const { result, cancel } = yield race({
-    result: call(
+  const { result, reset } = yield raceWithResetForecast(
+    call(
       forecastApi.getCityForecastForDate,
       action.payload.city,
       action.payload.date
-    ),
-    cancel: take(forecastUIActions.RESET_FORECAST),
-  });
-
-  if (!cancel) {
+    )
+  );
+  if (!reset) {
     yield put(forecastUIActions.setForecast(result));
   }
 }
 
+export function* getForecastDebounced(action) {
+  const { reset } = yield raceWithResetForecast(delay(1000));
+  if (!reset) {
+    yield getForecast(action);
+  }
+}
+
 export function* watchGetForecast() {
-  yield debounce(1000, forecastUIActions.GET_FORECAST, getForecast);
+  let task;
+  while (true) {
+    const action = yield take(forecastUIActions.GET_FORECAST);
+    if (task) {
+      yield cancel(task);
+    }
+    task = yield fork(getForecastDebounced, action);
+  }
 }
